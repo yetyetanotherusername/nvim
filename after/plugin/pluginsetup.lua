e5a4de31d1faaef08bc5e8f63a26df5dce5a3a57
Sebastian Grill <sebastian.grill@gmx.at>
2023-01-10 07:43:01 +0100

    applied formatter

--- after/plugin/pluginsetup.lua
+++ after/plugin/pluginsetup.lua
@@ -4,10 +4,10 @@ require('nvim-autopairs').setup({})
 
 local null_ls = require('null-ls')
 null_ls.setup({
-        sources = {
-                null_ls.builtins.formatting.isort,
-                null_ls.builtins.formatting.black
-        }
+  sources = {
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.black,
+  }
 })
 
 require('lualine').setup {
@@ -18,4 +18,3 @@ require('lualine').setup {
     section_separators = '',
   },
 }
-
