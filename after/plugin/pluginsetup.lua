a469eb74900465d9d014cb775f2d3c88a29b24a8
Sebastian Grill <sebastian.grill@atmoky.com>
2023-01-12 09:58:24 +0100

    added prettierd for yaml formatting

--- after/plugin/pluginsetup.lua
+++ after/plugin/pluginsetup.lua
@@ -8,7 +8,7 @@ null_ls.setup({
     null_ls.builtins.formatting.isort,
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.csharpier,
-
+    null_ls.builtins.formatting.prettierd,
   }
 })
 
