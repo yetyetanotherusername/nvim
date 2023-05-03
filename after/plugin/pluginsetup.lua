ed236228da61848c3e5b6c0bd9575685e81431fa
Sebastian Grill <sebastian.grill@atmoky.com>
2023-05-03 10:25:49 +0200

    feat: replaced isort with ruff

--- after/plugin/pluginsetup.lua
+++ after/plugin/pluginsetup.lua
@@ -6,7 +6,7 @@ require('nvim-autopairs').setup({})
 local null_ls = require('null-ls')
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.ruff,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.csharpier,
         null_ls.builtins.formatting.prettier,
