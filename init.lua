52f6cbf25eee47844afd8e61e8b82e8fa8b7f9f3
Sebastian Grill <sebastian.grill@atmoky.com>
2023-06-22 15:47:49 +0200

    feat: add markdown to default installed treesitter syntax

--- init.lua
+++ init.lua
@@ -48,11 +48,11 @@ require('packer').startup(function(use)
     use 'tpope/vim-rhubarb'
     use 'lewis6991/gitsigns.nvim'
 
-    use 'navarasu/onedark.nvim'             -- Theme inspired by Atom
-    use 'nvim-lualine/lualine.nvim'         -- Fancier statusline
+    use 'navarasu/onedark.nvim'               -- Theme inspired by Atom
+    use 'nvim-lualine/lualine.nvim'           -- Fancier statusline
     use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-    use 'numToStr/Comment.nvim'             -- "gc" to comment visual regions/lines
-    use 'tpope/vim-sleuth'                  -- Detect tabstop and shiftwidth automatically
+    use 'numToStr/Comment.nvim'               -- "gc" to comment visual regions/lines
+    use 'tpope/vim-sleuth'                    -- Detect tabstop and shiftwidth automatically
 
     -- Fuzzy Finder (files, lsp, etc)
     use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
@@ -221,7 +221,20 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'javascript', 'typescript', 'vimdoc', 'vim', 'c_sharp' },
+    ensure_installed = {
+        'c',
+        'cpp',
+        'go',
+        'lua',
+        'python',
+        'rust',
+        'javascript',
+        'typescript',
+        'vimdoc',
+        'vim',
+        'c_sharp',
+        'markdown'
+    },
 
     highlight = { enable = true },
     indent = { enable = false, disable = { 'python' } },
