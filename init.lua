39f3f211e6fc0e8cbe6a1339960e9e28856ad6ec
Sebastian Grill <sebastian.grill@atmoky.com>
2023-10-02 14:02:03 +0200

    remove offending ibl config from init.lua

--- init.lua
+++ init.lua
@@ -167,10 +167,7 @@ require('Comment').setup()
 
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
-require('ibl').setup {
-    char = '┊',
-    show_trailing_blankline_indent = false,
-}
+require('ibl').setup {}
 
 -- Gitsigns
 -- See `:help gitsigns.txt`
