d3847658dc2c8acff4158e090652b1739b5fa450
Sebastian Grill <sebastian.grill@atmoky.com>
2024-02-26 12:02:14 +0100

    chore(null-ls): ruff no longer requires null-ls format call

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,7 +4,6 @@ return {
         local null_ls = require('null-ls')
         null_ls.setup({
             sources = {
-                null_ls.builtins.formatting.ruff,
                 null_ls.builtins.formatting.csharpier,
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.cmake_format,
