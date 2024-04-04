f52840f1b665c1399821f7b1d151cc7156abbdbe
Sebastian Grill <sebastian.grill@atmoky.com>
2024-04-04 12:27:53 +0200

    feat(plugins): copilot enabled config

--- init.lua
+++ init.lua
@@ -689,6 +689,7 @@ require("lazy").setup({
                     --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
                 }),
                 sources = {
+                    { name = "copilot" },
                     { name = "nvim_lsp" },
                     { name = "luasnip" },
                     { name = "path" },
--- lua/plugins/copilot-cmp.lua
+++ lua/plugins/copilot-cmp.lua
@@ -1,6 +1,8 @@
 return {
---     "zbirenbaum/copilot-cmp",
---     config = function()
---         require("copilot_cmp").setup()
---     end
+    "zbirenbaum/copilot-cmp",
+    config = function()
+        require("copilot_cmp").setup({
+            fix_pairs = true,
+        })
+    end,
 }
