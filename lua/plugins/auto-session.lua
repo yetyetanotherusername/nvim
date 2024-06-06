582f1fbfffbc0b224798812e7cf47b3cb615a928
Sebastian Grill <sebastian.grill@atmoky.com>
2024-06-06 11:38:08 +0200

    feat(undotree): set focus on toggle
    - moved keymap to undotree.lua

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,5 +1,4 @@
 return {
-
     'rmagatti/auto-session',
     config = function()
         require('auto-session').setup({
--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,3 +1,7 @@
 return {
     "mbbill/undotree",
+    init = function()
+        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+        vim.cmd("let g:undotree_SetFocusWhenToggle = 1")
+    end
 }
