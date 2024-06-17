17a0711efbe86767bc2fff3d206d73240cb5935f
Sebastian Grill <sebastian.grill@atmoky.com>
2024-06-17 15:52:35 +0200

    feat(init): updated config
    - deactivated cursorline highlight
    - readded ibl settings
    - added stylua.toml to force space indentation

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,5 +1,12 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	main = "ibl",
-	opts = {},
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+        indent = { char = "┊" },
+        scope = {
+            show_end = false,
+            highlight = { "Function", "Label" },
+            char = "▎",
+        },
+    },
 }
