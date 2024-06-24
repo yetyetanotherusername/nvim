c04b02c1f150cc705073e0ff54b1abcf4972d920
Sebastian Grill <sebastian.grill@atmoky.com>
2024-06-24 13:47:38 +0200

    refactor(session-lens): merge setups
    - session-lens is part of auto-session now, so we merge the setups
    - open fugitive tab as post load hook in auto-session
    - minor refactoring

--- after/plugin/keybinds.lua
+++ after/plugin/keybinds.lua
@@ -5,9 +5,9 @@ vim.o.colorcolumn = "88"
 -- remaps
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open file browser" })
 vim.keymap.set("n", "<leader>tn", vim.cmd.tabnew, { desc = "Open new tab" })
-vim.keymap.set("n", "<leader>tc", vim.cmd.tabclose, { desc = "Close current tab" })
+vim.keymap.set("n", "<leader>tc", vim.cmd.tabclose, { desc = "[C]lose current [t]ab" })
 vim.keymap.set("n", "<leader>sc", require("telescope.builtin").commands, { desc = "[S]earch [C]ommands" })
-vim.keymap.set("n", "<leader>ss", require("session-lens").search_session, { desc = "Search session" })
+vim.keymap.set("n", "<leader>ss", require("auto-session.session-lens").search_session, { desc = "[S]earch [s]ession" })
 
 -- dap remaps
 vim.keymap.set("n", "<leader>b", require("dap").toggle_breakpoint, { desc = "Toggle breakpoint" })
@@ -17,11 +17,6 @@ vim.keymap.set("n", "<F7>", require("dap").step_into, { desc = "DAP step into" }
 vim.keymap.set("n", "<F8>", require("dap").step_out, { desc = "DAP step out" })
 vim.keymap.set("n", "<leader>du", require("dap").up, { desc = "DAP go one stackframe up" })
 vim.keymap.set("n", "<leader>dd", require("dap").down, { desc = "DAP go one stackframe down" })
-
--- open fugitive status in first tab
-if os.execute("git rev-parse --is-inside-work-tree") then
-	vim.cmd("-tab G | tabn")
-end
 
 -- open .mdx files as markdown
 vim.filetype.add({ extension = { mdx = "markdown" } })
--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,10 +1,19 @@
 return {
-    'rmagatti/auto-session',
+    "rmagatti/auto-session",
     config = function()
-        require('auto-session').setup({
+        require("auto-session").setup({
             path_display = { "shorten" },
             previewer = true,
-            auto_session_supress_dirs = { "~/", "~/Downloads", "/" }
+            auto_session_supress_dirs = { "~/", "~/Downloads", "/" },
+            session_lens = {},
+            post_restore_cmds = {
+                function()
+                    if os.execute("git rev-parse --is-inside-work-tree") then
+                        vim.cmd("0tab G | tabn")
+                    end
+                end,
+            },
         })
-    end
+        require("telescope").load_extension("session-lens")
+    end,
 }
