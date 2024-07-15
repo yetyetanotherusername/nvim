fc70e0a4df71e0e9ebd0630aade5dd24f7a93880
Sebastian Grill <sebastian.grill@atmoky.com>
2024-07-15 08:25:57 +0200

    chore(which-key): updated config

--- init.lua
+++ init.lua
@@ -191,19 +191,22 @@ require("lazy").setup({
             require("which-key").setup()
 
             -- Document existing key chains
-            require("which-key").register({
-                ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-                ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-                ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-                ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-                ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-                ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-                ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+            require("which-key").add({
+                { "<leader>c", group = "[C]ode" },
+                { "<leader>c_", hidden = true },
+                { "<leader>d", group = "[D]ocument" },
+                { "<leader>d_", hidden = true },
+                { "<leader>h", group = "Git [H]unk" },
+                { "<leader>h_", hidden = true },
+                { "<leader>r", group = "[R]ename" },
+                { "<leader>r_", hidden = true },
+                { "<leader>s", group = "[S]earch" },
+                { "<leader>s_", hidden = true },
+                { "<leader>t", group = "[T]oggle" },
+                { "<leader>t_", hidden = true },
+                { "<leader>w", group = "[W]orkspace" },
+                { "<leader>w_", hidden = true },
             })
-            -- visual mode
-            require("which-key").register({
-                ["<leader>h"] = { "Git [H]unk" },
-            }, { mode = "v" })
         end,
     },
 
@@ -767,7 +770,7 @@ require("lazy").setup({
             -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
             -- - sd'   - [S]urround [D]elete [']quotes
             -- - sr)'  - [S]urround [R]eplace [)] [']
-            require("mini.surround").setup()
+            -- require("mini.surround").setup()
 
             -- Simple and easy statusline.
             --  You could remove this setup call if you don't like it,
