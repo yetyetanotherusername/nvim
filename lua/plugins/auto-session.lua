efb0bae3b0cddcaf1970dd802f4de1e56a5108db
Sebastian Grill <sebastian.grill@atmoky.com>
2024-07-31 18:06:34 +0200

    refactor: simplified auto-session setup

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -3,11 +3,8 @@ return {
     config = function()
         require("auto-session").setup({
             path_display = { "shorten" },
-            previewer = true,
             auto_session_supress_dirs = { "~/", "~/Downloads", "/" },
             close_unsupported_windows = false,
-            session_lens = {},
         })
-        require("telescope").load_extension("session-lens")
     end,
 }
