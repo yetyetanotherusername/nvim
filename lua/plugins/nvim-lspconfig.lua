08a4a0205e18d647b20c2956f8608d9a8d99ff5c
Sebastian Grill <sebastian.grill@atmoky.com>
2025-04-24 10:48:44 +0200

    refactor(lspconfig): switch to new vim.lsp API

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -202,12 +202,9 @@ return { -- LSP Configuration & Plugins
             automatic_installation = false,
             handlers = {
                 function(server_name)
-                    local server = servers[server_name] or {}
-                    -- This handles overriding only values explicitly passed
-                    -- by the server configuration above. Useful when disabling
-                    -- certain features of an LSP (for example, turning off formatting for ts_ls)
-                    server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-                    require("lspconfig")[server_name].setup(server)
+                    local config = servers[server_name] or {}
+                    vim.lsp.config(server_name, config)
+                    vim.lsp.enable(server_name)
                 end,
             },
         })
