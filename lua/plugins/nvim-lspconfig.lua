5e42a05ad4cd0cd2758c1428a70bb384510d25a1
Sebastian Grill <sebastian.grill@atmoky.com>
2025-05-08 15:22:11 +0200

    chore: update mason repo path

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,8 +2,8 @@ return { -- LSP Configuration & Plugins
     "neovim/nvim-lspconfig",
     dependencies = {
         -- Automatically install LSPs and related tools to stdpath for Neovim
-        { "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
-        "williamboman/mason-lspconfig.nvim",
+        { "mason-org/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
+        "mason-org/mason-lspconfig.nvim",
         "WhoIsSethDaniel/mason-tool-installer.nvim",
 
         { "j-hui/fidget.nvim", opts = {} },
