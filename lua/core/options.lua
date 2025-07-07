52150016e4a1153f7a39a71ef198af1be2fdcc00
Sebastian Grill <sebastian.grill@atmoky.com>
2025-07-07 10:04:56 +0200

    refactor(options): removed uneeded diagnostics code

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -110,14 +110,5 @@ vim.diagnostic.config({
         -- error symbols & severity
         source = "if_many",
         spacing = 2,
-        format = function(diagnostic)
-            local diagnostic_message = {
-                [vim.diagnostic.severity.ERROR] = diagnostic.message,
-                [vim.diagnostic.severity.WARN] = diagnostic.message,
-                [vim.diagnostic.severity.INFO] = diagnostic.message,
-                [vim.diagnostic.severity.HINT] = diagnostic.message,
-            }
-            return diagnostic_message[diagnostic.severity]
-        end,
     },
 })
